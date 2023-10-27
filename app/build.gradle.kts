ad7bfa26228c3bc3e3ed7ac3de36e5fa84ac4e43
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2023-10-26 23:54:40 -0600

    Revert "Creating the layout for the fragment"
    This reverts commit 68b608a3aaa309c9965a5bbe4e1318b0dbef8a21.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.dwelventory"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.dwelventory"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/dwelventory/MainActivity.java
+++ app/src/main/java/com/example/dwelventory/MainActivity.java
@@ -2,47 +2,43 @@ package com.example.dwelventory;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity implements TagFragment.OnFragmentInteractionListener {
+import java.util.ArrayList;
 
-    private FragmentManager fragmentManager;
+public class MainActivity extends AppCompatActivity implements AddItemFragment.OnFragmentInteractionListener {
+
+    private ArrayList<Item> dataList;
+    private ListView itemList;
+    private ArrayAdapter<Item> itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
+        dataList = new ArrayList<>();
+
+        itemAdapter = new CustomList(this, dataList);
+        itemList = findViewById(R.id.item_list);
+        itemList.setAdapter(itemAdapter);
 
         final FloatingActionButton addButton = findViewById(R.id.add_item_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (savedInstanceState == null) {
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.setReorderingAllowed(true);
-                    fragmentTransaction.add(R.id.fragment_container_view_tag, new TagFragment(),null);
-                    fragmentTransaction.commit();
-                }
             }
         });
     }
 
-
     @Override
-    public void onClosePressed() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        return;
-    }
-
-    @Override
-    public void onAddPressed(Tag newTag) {
-       int i = 1;
+    public void onOKPressed(Item item) {
+        dataList.add(item);
+        itemAdapter.notifyDataSetChanged();
     }
 }
