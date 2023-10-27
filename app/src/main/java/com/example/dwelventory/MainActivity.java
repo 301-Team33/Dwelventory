759d0e665d5d35152807bd2e3e0906524434caee
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2023-10-26 18:19:51 -0600

    item class, basic activity layout, basic main
    missing custom list class so mainactiviity will be janky

--- app/src/main/java/com/example/dwelventory/MainActivity.java
+++ app/src/main/java/com/example/dwelventory/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.dwelventory;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AddItemFragment.OnFragmentInteractionListener {
+
+    private ArrayList<Item> dataList;
+    private ListView itemList;
+    private ArrayAdapter<Item> itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataList = new ArrayList<>();
+
+        itemAdapter = new CustomList(this, dataList);
+        itemList = findViewById(R.id.item_list);
+        itemList.setAdapter(itemAdapter);
+
+        final FloatingActionButton addButton = findViewById(R.id.add_item_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    @Override
+    public void onOKPressed(Item item) {
+        dataList.add(item);
+        itemAdapter.notifyDataSetChanged();
     }
 }
