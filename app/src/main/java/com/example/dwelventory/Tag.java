ccf03029c307fba44afcfdea825318c226f7613f
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2023-10-26 18:47:09 -0600

    Create Tag.java

---
+++ app/src/main/java/com/example/dwelventory/Tag.java
@@ -0,0 +1,17 @@
+package com.example.dwelventory;
+
+public class Tag {
+    String tagName;
+
+    public void Tag(String tagName){
+        this.tagName = tagName;
+    }
+
+    public String getTagName(){
+        return tagName;
+    }
+    
+    public void setTagName(String newName){
+        tagName = newName;
+    }
+}
