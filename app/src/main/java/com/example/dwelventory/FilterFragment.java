262e7ecb99b580aea0b47c9df3b2fb6ae905b9a2
Aykhan-229 <teymurlu@ualberta.ca>
2023-11-02 17:19:42 -0600

    added date and keyword

--- app/src/main/java/com/example/dwelventory/FilterFragment.java
+++ app/src/main/java/com/example/dwelventory/FilterFragment.java
@@ -12,6 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class FilterFragment extends DialogFragment {
@@ -21,6 +24,9 @@ public class FilterFragment extends DialogFragment {
 
     public interface FilterFragmentListener {
         void onMakeFilterApplied(String[] filterInput);
+        void onDateFilterApplied(Date start, Date end);
+        void onKeywordFilterApplied(String[] keywords);
+        void onTagFilterApplied(String[] tags);
     }
     public static FilterFragment newInstance(String filterOption) {
         FilterFragment fragment = new FilterFragment();
@@ -56,7 +62,79 @@ public class FilterFragment extends DialogFragment {
             return view;
         }
         else if("date".equals(filterOption)){   // For date filter
-            View view = inflater.inflate(R.layout.)
+            View view = inflater.inflate(R.layout.popup_select_drange, container, false);
+
+            EditText dateStart = view.findViewById(R.id.filter_date_cal1);
+            String dateStartText = dateStart.getText().toString();
+
+            EditText dateEnd = view.findViewById(R.id.filter_date_cal2);
+            String dateEndText = dateEnd.getText().toString();
+
+            Button doneButton = view.findViewById(R.id.filter_date_donebtn);
+
+            doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    if (listener != null) {
+
+                        Date start;
+                        Date end;
+                        try {
+                            start =new SimpleDateFormat("dd/MM/yyyy").parse(dateStartText);
+                            end =new SimpleDateFormat("dd/MM/yyyy").parse(dateEndText);
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        listener.onDateFilterApplied(start, end);
+                    }
+
+                    dismiss();
+                }
+            });
+            return view;
+        }
+        else if("keywords".equals(filterOption)) {
+            View view = inflater.inflate(R.layout.popup_select_keywords, container, false);
+
+            EditText keywordInput = view.findViewById(R.id.filter_kwords_etext);
+            String keywordText = keywordInput.getText().toString();
+            filterInput = keywordText.split(" ");
+
+            Button doneButton = view.findViewById(R.id.filter_kwords_donebtn);
+
+            doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    if (listener != null) {
+                        listener.onKeywordFilterApplied(filterInput);
+                    }
+
+                    dismiss();
+                }
+            });
+            return view;
+        }
+        else if("tags".equals(filterOption)) {
+            View view = inflater.inflate(R.layout.popup_select_tags, container, false);
+
+            EditText tagsInput = view.findViewById(R.id.filter_tags_etext);
+            String tagsText = tagsInput.getText().toString();
+            filterInput = tagsText.split(" ");
+
+            Button doneButton = view.findViewById(R.id.filter_tags_donebtn);
+
+            doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    if (listener != null) {
+                        listener.onTagFilterApplied(filterInput);
+                    }
+
+                    dismiss();
+                }
+            });
+            return view;
         }
         return null;
     }
