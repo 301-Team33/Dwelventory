39b7784af2ac4c5bc4697771b6206a15f151e628
Abhi-61 <abhisenthil01@gmail.com>
2023-11-02 15:52:27 -0600

    Checkbox and Select Items menu visible only on LongClick
    Delete Button, Select All Items and Count of selected items is yet to be implemented. But UI for the same is complete.

--- app/src/main/java/com/example/dwelventory/MainActivity.java
+++ app/src/main/java/com/example/dwelventory/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -54,12 +61,10 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private CollectionReference usersRef;
     private ArrayList<Item> dataList;
-<<<<<<< Updated upstream
-=======
-    private ArrayAdapter<Item> itemAdapter;
-    private ListView itemList;
 
->>>>>>> Stashed changes
+    //private ArrayAdapter<Item> itemAdapter;
+    //private ListView itemList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,34 +96,109 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
-        Item item1 = new Item("Billy",date1,"Pygmy Goat","Caramel w/ Black Markings",200);
+        Item item1 = new Item("Billy", date1, "Pygmy Goat", "Caramel w/ Black Markings", 200);
         Item item2 = new Item("Jinora", date2, "Pygmy Goat", "Caramel w/ Black Markings", 200);
         dataList.add(item1);
         dataList.add(item2);
 
 
-<<<<<<< Updated upstream
         ArrayAdapter<Item> itemAdapter = new ItemList(this, dataList);
         ListView itemList = findViewById(R.id.item_list);
         itemList.setAdapter(itemAdapter);
 
-=======
-        itemAdapter = new ItemList(this,dataList);
+        itemAdapter = new ItemList(this, dataList);
         itemList = findViewById(R.id.item_list);
         itemList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         itemList.setAdapter(itemAdapter);
 
+        // Declare itemList as new final variable
+        // (This variable is used only for the longClickListener)
+        final ListView finalItemList = itemList;
+        final ArrayAdapter<Item> finalItemAdapter = itemAdapter;
 
-        /*itemList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        itemList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                /*View checkBoxLayout = view.findViewById(R.id.checkbox);
+                checkBoxLayout.setVisibility(View.VISIBLE);*/
 
-                return false;
+                for (int j = 0; j < adapterView.getCount(); j++) {
+                    View view_temp = finalItemList.getChildAt(j);
+                    if (view_temp != null) {
+                        CheckBox checkBox = view_temp.findViewById(R.id.checkbox);
+                        checkBox.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                RelativeLayout select_items = findViewById(R.id.selectMultipleitems);
+                select_items.setVisibility(View.VISIBLE);
+                //changeListViewHeight(Boolean.TRUE);
+
+                ImageButton closebtn = findViewById(R.id.closebtn);
+                ImageButton deletebtn = findViewById(R.id.deletebtn);
+                closebtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        select_items.setVisibility(View.GONE);
+
+                        for (int j = 0; j < adapterView.getCount(); j++) {
+                            View view_temp = finalItemList.getChildAt(j);
+                            if (view_temp != null) {
+                                CheckBox checkBox = view_temp.findViewById(R.id.checkbox);
+                                checkBox.setVisibility(View.GONE);
+                            }
+                        }
+                    }
+                });
+
+                deletebtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        for (int j = 0; j < adapterView.getCount(); j++) {
+                            View view_temp = finalItemList.getChildAt(j);
+                            if (view_temp != null) {
+                                CheckBox checkBox = view_temp.findViewById(R.id.checkbox);
+                                //checkBox.setVisibility(View.GONE);
+                                if(checkBox.isChecked()){
+                                    finalItemAdapter.remove(dataList.get(j));
+                                    finalItemAdapter.notifyDataSetChanged();
+                                    checkBox.setChecked(false);
+                                    dataList.remove(j);
+                                }
+                            }
+                        }
+                        finalItemAdapter.notifyDataSetChanged();
+                    }
+                });
+                /*deletebtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int itemRemovedCount = 0;
+
+                        for (int j = dataList.size() - 1; j >= 0; j--) {
+                            Item currentItem = dataList.get(j);
+                            if (currentItem.isSelected()) {
+                                finalItemAdapter.remove(currentItem);
+                                dataList.remove(j);
+                                itemRemovedCount++;
+                            }
+                        }
+
+                        if (itemRemovedCount > 0) {
+                            Toast.makeText(MainActivity.this, "Deleted " + itemRemovedCount + " Items", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });*/
+                finalItemAdapter.notifyDataSetChanged();
+                return true;
+
             }
-        });*/
+        });
+        itemList = finalItemList;
+        itemAdapter = finalItemAdapter;
+        itemList.setAdapter(itemAdapter);
+        //itemAdapter.notifyDataSetChanged();
 
-
->>>>>>> Stashed changes
         final FloatingActionButton addButton = findViewById(R.id.add_item_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -126,6 +206,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+   /* private void changeListViewHeight(Boolean b, ListView itemList) {
+        if (b) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) itemList.getLayoutParams();
+            layoutParams.topMargin = 75;
+            itemList.setLayoutParams(layoutParams);
+            itemList.requestLayout();
+        } else {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) itemList.getLayoutParams();
+            layoutParams.topMargin = 0;
+            itemList.setLayoutParams(layoutParams);
+            itemList.requestLayout();
+        }
+    }*/
+
     @Override
     public void onStart() {
         super.onStart();
@@ -133,8 +228,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser == null) {
             signOnAnonymously();
-        }
-        else {
+        } else {
             Toast.makeText(MainActivity.this, "Already signed in",
                     Toast.LENGTH_SHORT).show();
             checkUsers(mAuth.getCurrentUser());
@@ -148,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
     }*/
 
     /**
-     *  This method will attempt to sign on anonymously, if the user is not already signed in
+     * This method will attempt to sign on anonymously, if the user is not already signed in
      */
     private void signOnAnonymously() {
         mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -173,8 +267,8 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method checks the Firestore database to see if a corresponding 'users' document exists
-     * @param user
-     *     This is the given user currently accessing the app/database
+     *
+     * @param user This is the given user currently accessing the app/database
      */
     private void checkUsers(FirebaseUser user) {
         DocumentReference doc = db.collection("users").document(user.getUid());
@@ -198,41 +292,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-<<<<<<< Updated upstream
-=======
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.multiple_items_selected,menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.multiple_items_selected, menu);
         return true;
         //return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        int id = item.getItemId();
-        int count = 0;
-        //final Button delete_btn = findViewById(R.id.)
-        if(id == R.id.delete_selected_items){
-            for(int i = 0; i<itemList.getCount(); i++){
-                if(itemList.isItemChecked(i)){
-                    itemAdapter.remove(dataList.get(i));
-                    dataList.remove(i);
-                    count++;
-                }
-            }
-        }
-        if(count != 0){
-            Toast.makeText(this, "Deleted "+ count+ " Items", Toast.LENGTH_LONG).show();
-        }
-        return super.onOptionsItemSelected(item);
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.multiple_items_selected, menu);
     }
-}
->>>>>>> Stashed changes
 
+}
     /*@Override
     public void onOKPressed(Item item) {
         dataList.add(item);
         itemAdapter.notifyDataSetChanged();
     }*/
-}
+
