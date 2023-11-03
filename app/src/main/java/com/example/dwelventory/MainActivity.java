7fa6a2f225f5a5082784daf96e7f6c0608099e88
Abhi-61 <abhisenthil01@gmail.com>
2023-11-02 18:04:17 -0600

    Resolved Activity_main layout issue
    Wasn't able to click on list items before. Resolved now.

--- app/src/main/java/com/example/dwelventory/MainActivity.java
+++ app/src/main/java/com/example/dwelventory/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
         dataList = new ArrayList<>();
 
-        ArrayList<Item> dataList = new ArrayList<>();
+        //ArrayList<Item> dataList = new ArrayList<>();
 
         // fake data
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
@@ -128,13 +128,14 @@ public class MainActivity extends AppCompatActivity {
                 /*View checkBoxLayout = view.findViewById(R.id.checkbox);
                 checkBoxLayout.setVisibility(View.VISIBLE);*/
 
-                for (int j = 0; j < adapterView.getCount(); j++) {
+                for (int j = 0; j < itemAdapter.getCount(); j++) {
                     View view_temp = finalItemList.getChildAt(j);
                     if (view_temp != null) {
                         CheckBox checkBox = view_temp.findViewById(R.id.checkbox);
                         checkBox.setVisibility(View.VISIBLE);
                     }
                 }
+
 
                 RelativeLayout select_items = findViewById(R.id.selectMultipleitems);
                 select_items.setVisibility(View.VISIBLE);
@@ -147,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View view) {
                         select_items.setVisibility(View.GONE);
 
-                        for (int j = 0; j < adapterView.getCount(); j++) {
+                        for (int j = 0; j < itemAdapter.getCount(); j++) {
                             View view_temp = finalItemList.getChildAt(j);
                             if (view_temp != null) {
                                 CheckBox checkBox = view_temp.findViewById(R.id.checkbox);
@@ -160,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
                 deletebtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        for (int j = 0; j < adapterView.getCount(); j++) {
+                        for (int j = 0; j < itemAdapter.getCount(); j++) {
                             View view_temp = finalItemList.getChildAt(j);
                             if (view_temp != null) {
                                 CheckBox checkBox = view_temp.findViewById(R.id.checkbox);
@@ -195,7 +196,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });*/
-                finalItemAdapter.notifyDataSetChanged();
+                //finalItemAdapter.notifyDataSetChanged();
                 return true;
 
             }
